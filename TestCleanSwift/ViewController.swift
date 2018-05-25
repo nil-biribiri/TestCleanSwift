16bdb304629c425a47ba1e3a6fc83f3d54dccb4d
Tanasak Ngerniam <nilc.nolan@gmail.com>
2018-05-25 15:51:36 +0700

    Initial Commit

---
+++ TestCleanSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestCleanSwift
+//
+//  Created by Tanasak.Nge on 25/5/2561 BE.
+//  Copyright © 2561 NilNilNil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
